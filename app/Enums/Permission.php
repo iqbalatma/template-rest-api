b880ed366a6cc80d60e9e630561ea6cae95a4256
iqbalatma <iqbalatma@gmail.com>
2024-02-24 21:29:54 +0700

    update management with namespacing feature group

--- app/Enums/Permission.php
+++ app/Enums/Permission.php
@@ -12,15 +12,15 @@ use ArchTech\Enums\Values;
 enum Permission:string {
     use Values;
     use Metadata;
-    #[Description("can show all data permissions")] #[FeatureGroup("permissions")]
+    #[Description("can show all data permissions")] #[FeatureGroup("management - permissions")]
     case MANAGEMENT_PERMISSIONS_SHOW = "management.permissions.show";
 
-    #[Description("can show all data roles")] #[FeatureGroup("roles")]
+    #[Description("can show all data roles")] #[FeatureGroup("management - roles")]
     case MANAGEMENT_ROLES_SHOW = "management.roles.show";
-    #[Description("can add new data roles")] #[FeatureGroup("roles")]
+    #[Description("can add new data roles")] #[FeatureGroup("management - roles")]
     case MANAGEMENT_ROLES_STORE = "management.roles.store";
-    #[Description("can update data roles by id")] #[FeatureGroup("roles")]
+    #[Description("can update data roles by id")] #[FeatureGroup("management - roles")]
     case MANAGEMENT_ROLES_UPDATE = "management.roles.update";
-    #[Description("can delete data roles by id")] #[FeatureGroup("roles")]
+    #[Description("can delete data roles by id")] #[FeatureGroup("management - roles")]
     case MANAGEMENT_ROLES_DESTROY = "management.roles.destroy";
 }
